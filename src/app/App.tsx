765ae3470046649b11aeb774aa2d7a80c2ca9088
profAlex <glomer@bk.ru>
2023-07-04 16:03:39 +0500

    Создание виджета Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 import './styles/index.scss';
 
@@ -12,15 +13,11 @@ export const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme]) }>
-            <button onClick={toggleTheme}>toggle theme</button>
-            <Link to='/'>
-                Главная
-            </Link>
-
-            <Link to='/about'>
-                О сайте
-            </Link> 
+            <Navbar />
             <AppRouter />
+            
+
+            <button onClick={toggleTheme}>toggle theme</button>
         </div>
     )
 }
