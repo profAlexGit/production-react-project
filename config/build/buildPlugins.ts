2ad4e3308d4a323fc4714a9b2411a33a9996a0b1
profAlex <glomer@bk.ru>
2023-07-05 14:09:39 +0500

    hmr and reactRefreshWebpackPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,8 +1,8 @@
-import path from 'path';
 import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import {BuildOptions} from '../types/config';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
 	return [
@@ -16,6 +16,8 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
 		}),
 		new webpack.DefinePlugin({
 			__IS_DEV__: isDev
-		})
+		}),
+		new ReactRefreshWebpackPlugin(),
+		new webpack.HotModuleReplacementPlugin()
 	]
 }
