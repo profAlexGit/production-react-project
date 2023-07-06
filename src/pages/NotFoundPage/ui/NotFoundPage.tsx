8b1ece7fb34cf04e30f535082f3d2ec14538d5c6
profAlex <glomer@bk.ru>
2023-07-06 11:46:09 +0500

    not found page

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,24 @@
+import type { FC } from 'react';
+import styles from './notFoundPage.module.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+
+interface NotFoundPageProps {
+	className?: string;
+}
+
+export const NotFoundPage: FC<NotFoundPageProps> = (props) => {
+	const {
+		className
+	} = props;
+
+	const { t } = useTranslation();
+
+	return (
+		<div
+			className={classNames(styles.notFoundPage, {}, [className])}
+		>
+			{t('Страница не найдена')}
+		</div>
+	);
+};
