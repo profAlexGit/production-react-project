4ddd7ee336a069ebd5083ecc677b167d629e5c1d
profAlex <glomer@bk.ru>
2023-07-08 15:14:32 +0500

    loki

--- src/features/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -1,1 +1,21 @@
-import type { Meta, StoryObj } from '@storybook/react';import { Theme } from 'app/providers/ThemeProvider';import { ThemeDecorator } from 'shared/config/storybookDecorators/ThemeDecorator';import { ThemeSwitcher } from './ThemeSwitcher';const meta: Meta<typeof ThemeSwitcher> = {  title: 'features/ThemeSwitcher',  component: ThemeSwitcher};export default meta;type Story = StoryObj<typeof ThemeSwitcher>;export const Light: Story = {};export const Dark: Story = {  decorators: [ThemeDecorator(Theme.DARK)]};
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybookDecorators/ThemeDecorator';
+import { ThemeSwitcher } from './ThemeSwitcher';
+
+const meta: Meta<typeof ThemeSwitcher> = {
+  title: 'features/ThemeSwitcher',
+  component: ThemeSwitcher
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ThemeSwitcher>;
+
+export const Light: Story = {
+};
+
+export const Dark: Story = {
+  decorators: [ThemeDecorator(Theme.DARK)]
+};
