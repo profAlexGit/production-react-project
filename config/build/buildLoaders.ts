ab1aafe638b8cb04ba6afecfaa2b092c5df1316e
profAlex <glomer@bk.ru>
2023-07-09 10:36:01 +0500

    fix pipeline

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,1 +1,46 @@
-import type webpack from 'webpack';import type { BuildOptions } from '../types/config';import { buildCssLoaders } from './loaders/buildCssLoaders';export function buildLoaders (options: BuildOptions): webpack.RuleSetRule[] {  const { isDev } = options;  const svgLoader = {    test: /\.svg$/i,    issuer: /\.[jt]sx?$/,    use: ['@svgr/webpack']  };  const fileLoader = {    test: /\.(png|jpe?g|gif)$/i,    use: [      {        loader: 'file-loader'      }    ]  };  const typescriptLoader = {    test: /\.tsx?$/,    use: 'ts-loader',    exclude: /node_modules/  };  const babelLoader = {    test: /\.(ts|js|tsx|jsx)$/,    exclude: /node_modules/,    use: {      loader: 'babel-loader'    }  };  const cssLoaders = buildCssLoaders(isDev);  return [    babelLoader,    typescriptLoader,    cssLoaders,    svgLoader,    fileLoader  ];}
+import type webpack from 'webpack';
+import type { BuildOptions } from '../types/config';
+import { buildCssLoaders } from './loaders/buildCssLoaders';
+
+export function buildLoaders (options: BuildOptions): webpack.RuleSetRule[] {
+  const { isDev } = options;
+
+  const svgLoader = {
+    test: /\.svg$/i,
+    issuer: /\.[jt]sx?$/,
+    use: ['@svgr/webpack']
+  };
+
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: 'file-loader'
+      }
+    ]
+  };
+
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/
+  };
+
+  const babelLoader = {
+    test: /\.(ts|js|tsx|jsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader'
+    }
+  };
+
+  const cssLoaders = buildCssLoaders(isDev);
+
+  return [
+    babelLoader,
+    typescriptLoader,
+    cssLoaders,
+    svgLoader,
+    fileLoader
+  ];
+}
