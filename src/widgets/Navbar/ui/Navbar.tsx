cb5581e1fef6d10773c70e6fad3285bcf3c66496
profAlex <glomer@bk.ru>
2023-07-10 14:47:39 +0500

    fix lint errors

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,15 +1,12 @@
 import type { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import styles from './navbar.module.scss';
-import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
   className?: string
 }
 
 export const Navbar: FC<NavbarProps> = ({ className }) => {
-  const { t } = useTranslation('navigation');
-
   return (
     <div className={classNames(styles.navbar, {}, [className])}>
 
