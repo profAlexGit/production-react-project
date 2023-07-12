10955ebe15d010e1542778f0c5c6965165b0cdbb
profAlex <glomer@bk.ru>
2023-07-12 10:01:17 +0500

    add modal, portal. Change Navbar. Add job screenshots test to github actions

---
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Modal } from 'shared/ui/Modal/Modal';
+
+const meta: Meta<typeof Modal> = {
+  title: 'shared/Modal',
+  component: Modal,
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid autem cum dignissimos dolorum ducimus excepturi expedita inventore ipsum minima nisi optio perferendis perspiciatis quibusdam quo repellat sit, voluptate voluptatibus!',
+    isOpen: true
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Modal>;
+
+export const ModalDefault: Story = {
+
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,15 +1,35 @@
 import type { FC } from 'react';
+import { useState, useCallback } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import styles from './navbar.module.scss';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { useTranslation } from 'react-i18next';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 interface NavbarProps {
   className?: string
 }
 
 export const Navbar: FC<NavbarProps> = ({ className }) => {
+  const { t } = useTranslation();
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal(prev => !prev);
+  }, []);
+
   return (
     <div className={classNames(styles.navbar, {}, [className])}>
-
+      <Button
+        theme={ThemeButton.CLEAR_INVERTED}
+        className={styles.links}
+        onClick={onToggleModal }
+      >
+        {t('Войти')}
+      </Button>
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid autem cum dignissimos dolorum ducimus excepturi expedita inventore ipsum minima nisi optio perferendis perspiciatis quibusdam quo repellat sit, voluptate voluptatibus!
+      </Modal>
     </div>
   );
 };
